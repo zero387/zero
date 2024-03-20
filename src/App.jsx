65e6b48e44262a308174f34cbbfa94e3e64d20a1
zero387 <88674361+zero387@users.noreply.github.com>
2024-03-20 22:13:31 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,75 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { up1 } from './redux-toolkit/reducers/counter';
-import { addTodo,deleteJob } from './redux-toolkit/reducers/todolist';
-
-
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addJob, deleteJob ,  changeStatus } from './redux/reducers/todoList';
 
 export default function App() {
-  const [data, setData] = useState({
-    name:"",
-    id:''
+  const [job, setJob] = useState({
+    name: '',
+    id: '',
+    status: false
   });
-  const count = useSelector((state) => {
-    console.log(state);
-    return state;
+  const [jobs, setJobs] = useState([]);
+
+  const handleChange = (event) => {
+    setJob({
+      name: event.target.value,
+      id: '',
+      status: false,
+    });
+  }
+
+  const getJobs = useSelector((state) => {
+    return state.todoList
   });
   const dispatch = useDispatch();
-
-  const increaseCounter = () => {
-    dispatch(up1());
-  };
-
-  const addToDoList = () => {
-    dispatch(addTodo(data));
-  };
-
-  const handleChange = (e) => {
-    setData({...data,name:e.target.value,id: Math.floor(Math.random()*100000)});
-  };
-
-  const deleteJob1=(id)=>{
-      // bạn có muốn xóa không 
-      //console.log(id);
-      //const check1 = confirm('bạn có muốn xóa hay không')
-      //console.log(check1);
-
-      dispatch(deleteJob(id))
-      
+  const addTodo = () => {
+    dispatch(addJob(job));
+    setJob({
+      name: '',
+      id: '',
+      status: false
+    });
   }
+
+  const handleCheckboxChange = (id) => {
+   dispatch( changeStatus(id))
+    
+  
+  };
+
   return (
     <div>
-      <p>Counter value: {count.counter11.value}</p>
-      <button onClick={increaseCounter}>Increase Counter</button>
-      <input type="text" onChange={handleChange} />
-      <button onClick={addToDoList}>Add to To Do List</button>
+      <p>todolist</p>
+      <input 
+        type="text"
+        value={job.name}
+        onChange={handleChange}
+      />
+      <button onClick={addTodo}>add</button>
       <ul>
-        {count.todolist11.map((item,index)=>{
-        return <li key={index}>
-                  {item.name}  <button
-                          onClick={()=>deleteJob1(item.id)}
-                          >delete</button>
+        {
+          getJobs.map((item, index) => {
+            return (
+              <li key={index} >
+              <p style={{ textDecoration: item.status ? 'line-through' : '' }}>{item.name}</p>
+
+                
+                <input 
+                  type='checkbox'
+                  
+                  onChange={() => handleCheckboxChange(item.id)}
+                />
+                <span 
+                  className="material-symbols-outlined"
+                  onClick={() => dispatch(deleteJob(index ))}
+                >
+                  delete
+                </span>
               </li>
-        
-      })}
+            );
+          })
+        }
       </ul>
-      
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 //import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './redux-toolkit/store/store.js'
-
+import { store } from './redux/store/store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
- 
-
   <Provider store={store}>
-
-    <App />
+   <App />
   </Provider>
-    
-  
+   
+
 )
